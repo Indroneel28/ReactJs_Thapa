c2ec38d81bb9be0980d5b226b5e60b2a18669393
Indroneel28 <122139794+Indroneel28@users.noreply.github.com>
2025-07-27 23:44:59 +0530

    Pokemon Game done

--- Pokemon/src/Pokemon.jsx
+++ Pokemon/src/Pokemon.jsx
@@ -1,29 +1,83 @@
-import React from 'react'
-import './index.css'
-import { useEffect } from 'react'
+import React, { useState } from "react";
+import "./index.css";
+import { useEffect } from "react";
+import PokemonCard from "./pages/PokemonCard";
 
 const Pokemon = () => {
-    const API= "https://pokeapi.co/api/v2/pokemon?limit=24";
+  const API = "https://pokeapi.co/api/v2/pokemon?limit=100";
+  const [pokemon, setPokemon] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [search, setSearch] = useState("");
 
-    const fetchPokemon= async() => {
-        try{
-            const res= await fetch(API);
-            //fetch always returns a promise
-            const  data= await res.json();
-            console.log(data)
-        }
-        catch(error){
+  const fetchPokemon = async () => {
+    try {
+      const res = await fetch(API);
+      //fetch always returns a promise
+      const data = await res.json();
+      //console.log(data);
 
-        }
+      const detailedPokemonData = data.results.map(async (currPokemon) => {
+        const res = await fetch(currPokemon.url);
+        const data = await res.json();
+        //console.log(data);
+        return data;
+      });
+
+      const detailedResponses = await Promise.all(detailedPokemonData);
+      console.log(detailedResponses);
+      setPokemon(detailedResponses);
+      setLoading(false);
+    } catch (error) {
+      setError(true);
     }
+  };
 
-    useEffect(()=>{
-        fetchPokemon();
-    },[])
+  useEffect(() => {
+    fetchPokemon();
+  }, []);
 
+
+  //Search functionality
+  const searchData= pokemon.filter((currPokemon) => {
+    return currPokemon.name.toLowerCase().includes(search.toLowerCase());
+  })
+
+
+  if (error) {
+    return <h1>Something went wrong</h1>;
+  } else if (loading) {
+    return <h1>Loading...</h1>;
+  } else {
     return (
-        <div>Pokemon</div>
-    )
-}
+      <>
+        <section className="container">
+          <header>
+            <h1>Lets Catch Pokemon</h1>
+          </header>
 
-export default Pokemon
+          <div className="pokemon-search">
+            <input
+              type="text"
+              placeholder="Search Pokemon"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+          </div>
+
+          <div>
+            <ul className="cards">
+              {searchData.map((currPokemon) => {
+                return (
+                  <PokemonCard key={currPokemon.id} pokemonData={currPokemon} />
+                );
+              })}
+            </ul>
+          </div>
+        </section>
+      </>
+    );
+  }
+};
+
+export default Pokemon;
